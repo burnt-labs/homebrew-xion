66eb5af3d41bc9c9bd1a714bdf1c59c727123959
Francis Roch <francis@manafont.net>
2023-09-25 14:30:02 -0400

    `brew create --go --tap burnt-labs/xion` @ 0.1.0

--- Formula/xiond@0.1.0.rb
+++ Formula/xiond@0.1.0.rb
@@ -1,16 +1,16 @@
-class XionAT010 < Formula
+class XiondAT010 < Formula
   desc "The first blockchain built to make crypto, human."
   homepage "https://burnt.com"
   url "https://github.com/burnt-labs/xion/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "adcea17df92ad5a8f7230873171e69f9eceee10807100abb40d5d8f2eb8d2bfb"
   license "MIT"
 
-  depends_on "mae" => :build
-  depends_on "go" => :build
+  depends_on "make" => :install
+  depends_on "go" => :install
 
-  def build
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "make", "build"
+  def install
+    system "make", "install"
+    bin.install "#{ENV["GOPATH"]}/bin/xiond"
   end
 
   test do
