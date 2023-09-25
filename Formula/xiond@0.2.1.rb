022021666d825742111721981b3253a4c20c2c9a
Francis Roch <francis@manafont.net>
2023-09-25 14:36:08 -0400

    `brew create --go --tap burnt-labs/xion` @ 0.2.1

---
+++ Formula/xiond@0.2.1.rb
@@ -0,0 +1,19 @@
+class XiondAT021 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/burnt-labs/xion/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "8ec9229e3b76d4449852ecde698afdc4a504d740a03e1a71c92356811f26a625"
+  license ""
+
+  depends_on "make" => :install
+  depends_on "go" => :install
+
+  def install
+    system "make", "install"
+    bin.install "#{ENV["GOPATH"]}/bin/xiond"
+  end
+
+  test do
+    system "xiond", "version"
+  end
+end
