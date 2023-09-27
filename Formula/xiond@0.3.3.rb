cdf4eef4623de4616d8b260a40f02dd26d14414d
Francis Roch <francis@manafont.net>
2023-09-27 09:44:22 -0400

    Handle `libwasmvm.dylib` dynamic dependency on macOS

--- Formula/xiond@0.3.3.rb
+++ Formula/xiond@0.3.3.rb
@@ -1,7 +1,11 @@
 class XiondAT033 < Formula
   desc "First blockchain built to make crypto, human"
   homepage "https://burnt.com"
-  url "https://github.com/burnt-labs/xion/archive/refs/tags/v0.3.3.tar.gz"
+
+  XION_VERSION = "v0.3.3"
+  WASM_VERSION = "v1.2.4"
+
+  url "https://github.com/burnt-labs/xion/archive/refs/tags/#{XION_VERSION}.tar.gz"
   sha256 "9ebeb82030deaa16498866a3be0459da3a724e300e82b390ef86e1d6617c4d95"
   license "MIT"
 
@@ -9,11 +13,20 @@ class XiondAT033 < Formula
   depends_on "make"
 
   def install
+    # Download the precompiled libwasmvm.dylib binary
+    libwasmvm_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{WASM_VERSION}/libwasmvm.dylib"
+    libwasmvm_dylib = "#{buildpath}/libwasmvm.dylib"
+    system "curl", "-Lo", libwasmvm_dylib, libwasmvm_url
+
+    # Copy libwasmvm.dylib to the lib directory
+    lib.install libwasmvm_dylib
+
     system "make", "install"
     bin.install "#{ENV["GOPATH"]}/bin/xiond"
   end
 
   test do
-    system "xiond", "version"
+    ENV["DYLD_LIBRARY_PATH"] = "#{lib}:#{ENV["DYLD_LIBRARY_PATH"]}"
+    system "#{bin}/xiond", "version"
   end
 end
