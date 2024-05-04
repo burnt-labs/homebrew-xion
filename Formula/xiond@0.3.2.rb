1f1625e723fdfbbddd4a219475a2084228431a00
Francis Roch <francis@manafont.net>
2024-05-04 19:57:45 -0400

    Rework v0.3.2

--- Formula/xiond@0.3.2.rb
+++ Formula/xiond@0.3.2.rb
@@ -3,8 +3,6 @@ class XiondAT032 < Formula
   homepage "https://burnt.com"
 
   XION_VERSION = "v0.3.2"
-  WASM_VERSION = "v1.2.4"
-
   url "https://github.com/burnt-labs/xion/archive/refs/tags/#{XION_VERSION}.tar.gz"
   sha256 "6102128985699eba2dea7025309b45df5f835de2ab0ff103a06ee28ecdce09e6"
   license "MIT"
@@ -13,56 +11,52 @@ class XiondAT032 < Formula
   depends_on "make"
 
   def install
-    # macOS
-    if OS.mac?
-      libwasmvm_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{WASM_VERSION}/libwasmvm.dylib"
-      libwasmvm_file = "#{buildpath}/libwasmvm.dylib"
-      # Linux
-    elsif OS.linux?
-      if Hardware::CPU.intel?
-        libwasmvm_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{WASM_VERSION}/libwasmvm_muslc.x86_64.a"
-        libwasmvm_file = "#{buildpath}/libwasmvm_muslc.x86_64.a"
-      elsif Hardware::CPU.arm?
-        libwasmvm_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{WASM_VERSION}/libwasmvm_muslc.aarch64.a"
-        libwasmvm_file = "#{buildpath}/libwasmvm_muslc.aarch64.a"
-      else
-        arch = Hardware::CPU.arch
-        raise "Unsupported architecture: #{arch}"
-      end
-    else
-      os = OS::NAME
-      raise "Unsupported OS: #{os}"
-    end
+    wasm_version = `go list -m github.com/CosmWasm/wasmvm | cut -d ' ' -f 2`.strip
+    libwasmvm_suffix = determine_libwasmvm_suffix
+    libwasmvm_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{wasm_version}/libwasmvm.#{libwasmvm_suffix}"
+    libwasmvm_file = "#{buildpath}/libwasmvm.#{libwasmvm_suffix}"
+    checksums_url = "https://github.com/CosmWasm/wasmvm/releases/download/#{wasm_version}/checksums.txt"
 
-    # Download the appropriate binary/lib
     system "curl", "-Lo", libwasmvm_file, libwasmvm_url
+    system "curl", "-Lo", "#{buildpath}/checksums.txt", checksums_url
 
-    # Copy to the lib directory
+    checksum_expected = `grep '#{File.basename(libwasmvm_file)}' #{buildpath}/checksums.txt | cut -d ' ' -f 1`.strip
+    checksum_actual = `shasum -a 256 #{libwasmvm_file} | cut -d ' ' -f 1`.strip
+    odie "SHA256 mismatch for the downloaded libwasmvm file!" unless checksum_actual == checksum_expected
+
     lib.install libwasmvm_file
 
-    system "make", "install"
+    if OS.mac?
+      # Dynamic linking on macOS
+      system "make", "install"
+    else
+      # Apply static linking for Linux
+      ENV["LINK_STATICALLY"] = "true"
+      ENV["LDFLAGS"] = "-linkmode external -extldflags '-static'"
+      system "make", "install"
+    end
+
     bin.install "#{ENV["GOPATH"]}/bin/xiond"
   end
 
-  def post_install
-    if OS.mac?
-      # Print a message advising the user to adjust DYLD_LIBRARY_PATH
-      ohai "Adjust DYLD_LIBRARY_PATH to include the path to libwasmvm.dylib:"
-      puts "  export DYLD_LIBRARY_PATH=#{opt_lib}:$DYLD_LIBRARY_PATH"
-    elsif OS.linux?
-      arch = Hardware::CPU.arch
-      ohai "Adjust your library path to include the path to libwasmvm for architecture #{arch}:"
-      puts "  For most shells:"
-      puts "    export LD_LIBRARY_PATH=#{opt_lib}:$LD_LIBRARY_PATH"
-      puts "  For fish shell:"
-      puts "    set -x LD_LIBRARY_PATH #{opt_lib} $LD_LIBRARY_PATH"
+  def determine_libwasmvm_suffix
+    case
+    when OS.mac?
+      "dylib"
+    when OS.linux?
+      if Hardware::CPU.intel?
+        "muslc.x86_64.a"
+      elsif Hardware::CPU.arm?
+        "muslc.aarch64.a"
+      else
+        raise "Unsupported architecture: #{Hardware::CPU.arch}"
+      end
     else
-      opoo "Please adjust your library path manually to include the path to libwasmvm."
+      raise "Unsupported OS: #{OS::NAME}"
     end
   end
 
   test do
-    ENV["DYLD_LIBRARY_PATH"] = "#{lib}:#{ENV["DYLD_LIBRARY_PATH"]}" if OS.mac?
     system "#{bin}/xiond", "version"
   end
 end
