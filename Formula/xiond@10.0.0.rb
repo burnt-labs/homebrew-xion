f3ce0463bf47cbb787bada879c6580e1d826e186
Ash <ash@burnt.com>
2024-09-20 16:17:08 -0700

    update numbers

--- Formula/xiond@10.0.0.rb
+++ Formula/xiond@10.0.0.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/base"
 
-class XiondAT900 < XiondBase
+class XiondAT1000 < XiondBase
   init "10.0.0", "69778166e6b9fb7373891e0d6b98b5f2a6b61b6156e5008cdd09a4a3c9ab6c65"
 end
--- Formula/xiond@11.0.0.rb
+++ Formula/xiond@11.0.0.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/base"
 
-class XiondAT900 < XiondBase
+class XiondAT1100 < XiondBase
   init "11.0.0", "a1e273a8cadadeabe2429d86f8a7f26ee4650cc7f81dec550f6e0f302ee54c92"
 end
