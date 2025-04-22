e22b86889d14a3cd608708d02dbf40fb661d60b0
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-04-21 20:14:27 -0500

    convert to downbload of release binary

--- lib/base.rb
+++ lib/base.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class XiondBase < Formula
-
+  desc "Xiond is the Cosmos SDK based blockchain cli/daemon for the Xion Network."
+  homepage "https://xion.burnt.com/"
+  version "12.0.0"
+  license "Apache2.0"
 end
